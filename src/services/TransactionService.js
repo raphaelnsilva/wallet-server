13722e6c8e877e1571f500febfd836073c4c68da
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-09-13 20:35:28 -0300

    fix transactionsServer

--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -1,43 +1,42 @@
+import { v4 as uuidv4 } from "uuid";
 import Cart from '../models/Cart'
 import Transaction from "../models/Transaction"
 
 class TransactionService {
-    async process({
-        cartCode,
-        paymentType,
-        installments,
-        customer,
-        billing,
-        creditCard,
-    }) {
-        const cart = Cart.findOne({ cartCode });
-
-        if(!cart) {
-            throw `Cart ${cartCode} was not found.`;
-        }
-
-        const transaction = await Transaction.create({
-            cartCode: cart.code,
-            code: 'abc123',
-            total: cart.price,
-            paymentType,
-            installments,
-            status: "started",
-            customerName: customer.name,
-            customerEmail: customer.email,
-            customerMobile: customer.mobile,
-            customerDocument: customer.document,
-            billingAddress: billing.address,
-            billingNumber: billing.number,
-            billingNeighborhood: billing.neighborhood,
-            billingCity: billing.city,
-            billingState: billing.state,
-            billingZipCode: billing.zipcode,
-        });
-        
-        return transaction;
-
+  async process({
+    cartCode,
+    paymentType,
+    installments,
+    customer,
+    billing,
+    creditCard,
+  }) 
+  
+  {
+    const cart = await Cart.findOne({ code: cartCode });
+    if(!cart) {
+      throw `Cart was not found.`;
     }
+    const transaction = await Transaction.create({
+      cartCode: cart.code,
+      code: await uuidv4(),
+      total: cart.price,
+      paymentType,
+      installments,
+      status: "started",
+      customerName: customer.name,
+      customerEmail: customer.email,
+      customerMobile: customer.mobile,
+      customerDocument: customer.document,
+      billingAddress: billing.address,
+      billingNumber: billing.number,
+      billingNeighborhood: billing.neighborhood,
+      billingCity: billing.city,
+      billingState: billing.state,
+      billingZipCode: billing.zipcode,
+    });
+    return transaction;
+  }
 }
 
 export default TransactionService;
